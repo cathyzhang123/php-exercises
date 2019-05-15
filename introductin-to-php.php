370ac5f32c5423f6dd59e270b1c70a1a82f1bebc
Cathy Zhang <50335806+cathyzhang123@users.noreply.github.com>
2019-05-14 20:22:06 -0400

    Online Class: Introduction to PHP
    MTM1531_010 Database and Server side development - Online Class: Introduction to PHP

--- introductin-to-php.php
+++ introductin-to-php.php
@@ -1,64 +1,89 @@
 <?php
 
-const BR = "<br>";
-/*====================================
-1. Print the sentence: Welcome to PHP!
-=======================================*/ 
-    echo "<em><strong>Welcome to PHP!</strong></em>".BR;   
+/*
++---+
+| 1 | 
++---+
+Print the sentence: Welcome to PHP!
+*/
+echo "<em><strong>Welcome to PHP!</strong></em>";
+
 
 // task separator
-    echo "<hr style=\"margin 1rem 0\">";
+echo "<hr style=\"margin 1rem 0\">";
 
-/*====================================
-2. Print the sentence: PHP stands for "Hypertext Preprocessor"!
-======================================*/
-    $phpmeaning = 'PHP stands for "Hypertext Preprocessor"!';
-    echo "<em><strong>".$phpmeaning."</strong></em>".BR;   
+/* 
++---+
+| 2 | 
++---+
+Print the sentence: PHP stands for "Hypertext Preprocessor"!
+*/
+$phpmeaning = 'PHP stands for "Hypertext Preprocessor"!';
+echo "<em><strong>".$phpmeaning."</strong></em>";
 
-// task separator
-    echo "<hr style=\"margin 1rem 0\">";
-
-/*====================================
-3. Print the following sentence as a paragraph: PHP stands for "Hypertext Preprocessor"!
-====================================*/
-    echo "<p><em><strong>".$phpmeaning."</strong></em></p>";
 
 // task separator
-    echo "<hr style=\"margin 1rem 0\">";
+echo "<hr style=\"margin 1rem 0\">";
 
-/*====================================
-4. Print the following acronyms - every acronym in a new line:
+/* 
++---+
+| 3 | 
++---+
+Print the following sentence as a paragraph: PHP stands for "Hypertext Preprocessor"!
+*/
+ echo "<p><em><strong>".$phpmeaning."</strong></em></p>";
+
+
+// task separator
+echo "<hr style=\"margin 1rem 0\">";
+
+/* 
++---+
+| 4 | 
++---+
+Print the following acronyms - every acronym in a new line:
+
 HTML - Stands for Hypertext Markup Language
 CSS - Stands for Cascading Stylesheet
 JS - Stands for JavaScript
 PHP - Stands for Hypertext Preprocessor
 SQL - Stands for Structural Query Language
-====================================*/
-echo "<em><strong>HTML - Stands for Hypertext Markup Language</strong></em>".BR;
-echo "<em><strong>CSS - Stands for Cascading Stylesheet</strong></em>".BR;
-echo "<em><strong>JS - Stands for JavaScript</strong></em>".BR;
-echo "<em><strong>PHP - Stands for Hypertext Preprocessor</strong></em>".BR;
-echo "<em><strong>SQL - Stands for Structural Query Language</strong></em>".BR;      
+*/
+$html = "HTML - Stands for Hypertext Markup Language";
+$css = "CSS - Stands for Cascading Stylesheet";
+$js = "JS - Stands for JavaScript";
+$php = "PHP - Stands for Hypertext Preprocessor";
+$sql = "SQL - Stands for Structural Query Language";
+echo "<em><strong>".$html."</strong></em><br>";
+echo "<em><strong>".$css."</strong></em><br>";
+echo "<em><strong>".$js."</strong></em><br>";
+echo "<em><strong>".$php."</strong></em><br>";
+echo "<em><strong>".$sql."</strong></em>";   
+   
+   
    
 // task separator
 echo "<hr style=\"margin 1rem 0\">";
 
-/*====================================
-5. Print the following acronyms in unordered list - <ul> ... </ul>:
+/* 
++---+
+| 5 | 
++---+
+Print the following acronyms in unordered list - <ul> ... </ul>:
+
 HTML - Stands for Hypertext Markup Language
 CSS - Stands for Cascading Stylesheet
 JS - Stands for JavaScript
 PHP - Stands for Hypertext Preprocessor
 SQL - Stands for Structural Query Language
-====================================*/  
+*/
 $list="<ul><em><strong>
-    <li>HTML - Stands for Hypertext Markup Language</li>
-    <li>CSS - Stands for Cascading Stylesheet</li>
-    <li>JS - Stands for JavaScript</li>
-    <li>PHP - Stands for Hypertext Preprocessor</li>
-    <li>SQL - Stands for Structural Query Language</li>
+    <li>".$html."</li>
+    <li>".$css."</li>
+    <li>".$js."</li>
+    <li>".$php."</li>
+    <li>".$sql."</li>
     </strong></em></ul>";
 
 echo $list;
-
 ?>
