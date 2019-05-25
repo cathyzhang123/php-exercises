a53cd8c6f55deca869a2a2a93bb5733222722d69
Cathy Zhang <50335806+cathyzhang123@users.noreply.github.com>
2019-05-25 09:31:24 -0400

    Update variables.php

--- variables.php
+++ variables.php
@@ -14,27 +14,21 @@ Declare the variable named language and
 assign it with value PHP.
 */
 $language = "PHP";
-
 /*
 Combine (chain or concatenate) language 
 with the string literal to create the expression 
 which will output the sentence: 
 Welcome to PHP!. 
-
 Declare the variable greeting, 
 assign it with the created expression.
 */
-$greeting = "Welcome to ".$language."!";
-
+$greeting = "Welcome to {$language}!";
 /*
 Print greeting.
 */
-echo "<strong><em>".$greeting."<em><strong>";
-
-
+echo $greeting;
 // task separator
 echo "<hr style=\"margin 1rem 0\">";
-
 /* 
 +---+
 | 2 |
@@ -43,50 +37,40 @@ Declare variable named firstName and
 assign it with the value of your first name.
 */
 $firstName = "Cathy";
-
 /*  
 Declare variable named lastName and 
 assign it with the value of your last name.
 */
 $lastName = "Zhang";
-
 /*
 Declare variable named day and 
 assign it with the day of your birthday (number).
 */
 $day = 12;
-
 /*
 Declare variable named month and 
 assign it with the month of your birthday (string).
 */
 $month = "February";
-
 /*
 Declare variable named year and 
 assign it with the year of your birthday (number).
 */
 $year = 1971;
-
 /*
 Use all of the variables above and create expression that will
 output the sentence reading:
 My name is _ _ _ _ _  _ _ _ _ _. I was born on _ _ of _ _ _ _ _ in  _ _ _ _.
-
 Declare variable named assembled and 
 assign it with the chained (concatenated) expression above.
 */
 $assembled = "My name is ".$firstName." ".$lastName.".  I was born on ".$day." of ".$month." in ".$year.".";
 /*
-
 Print assembled.
 */    
 echo $assembled;
-
-
 // task separator
 echo "<hr style=\"margin 1rem 0\">";
-
 /*
 +---+
 | 3 |
@@ -101,37 +85,30 @@ Calculate and print the area of the rectangle below:
 Declare the variable length and assign it with the specified value.
 */
 $length = 6.5;
-
 /*
 Declare the variable width and assign it with the specified value.
 */
 $width = 3.5;
-
 /*
 Declare the variable area and assign it with the product of length and width.
 */
 $area = $length * $width;
-
 /*
 Print area.
 */
 echo $area;
-
 // task separator
 echo "<hr style=\"margin 1rem 0\">";
-
 /*
 +---+
 | 4 |
 +---+
 Description: Printing the price of product including the tax and delivery.
 */
-
 /*
 Declare the variable product and assign it with armchair.
 */
 $product = "armchair";
-
 /*
 Declare the variable price and assign it with 249.00.
 */
@@ -141,29 +118,23 @@ $price = number_format(249, 2);
 Declare the variable tax and assign it with 13% (0.13).
 */
 $tax = 0.13;
-
 /*
 Declare the variable delivery and assign it with 5% (0.05).
 */
 $delivery = 0.05;
-
 /*
 Use price, tax and delivery 
 to create the expression that will output the price after the tax and delivery.
-
 Declare the variable total and assign it with the expression gotten from the previous step.
 */
 $total = ($price  + $price * $delivery) * (1 + $tax );
-
 /*
 Concatenate the variables and string literals to print the statement:
 armchair: $295.44
 */
 echo "armchair: ".number_format($total, 2);
-
 // task separator
 echo "<hr style=\"margin 1rem 0\">";
-
 /*
 +---+
 | 5 |
@@ -180,7 +151,7 @@ should be contained by figcaption element):
 
 <figure>
   <img src="https://picsum.photos/id/788/300/300" alt="placeholder-image">
-  <figcaption><strong><em><?php echo "armchair: ".number_format($total, 2); ?></em></strong></figcaption>
+  <figcaption><?php echo "armchair: ".number_format($total, 2); ?></figcaption>
 </figure>
 
 </body>
